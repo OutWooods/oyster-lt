1323f35321f4f883b5595deab63438fbffb09987
Leigh-ann <leighann.gant@gmail.com>
2018-01-08 15:13:11 +0000

    Feature added to allow top-up

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -5,4 +5,8 @@ class OysterCard
     @balance = 0
   end
 
+  def top_up(amount)
+    @balance += amount
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,13 +1,18 @@
 require 'oyster_card'
 
 describe OysterCard do
- subject(:card) {described_class.new}
+ subject(:card) { described_class.new }
 
- # it "has a balance which is a float" do
- #   expect(card.balance).to be_an_instance_of(Float)
- # end
-
- it "has a balance of 5" do
+ it "has a balance" do
    expect(card.balance).to eq 0
+ end
+ describe '#top_up' do
+   it 'increases balance when topped up' do
+     expect { card.top_up(5) }.to change { card.balance }.by (5)
+   end
+   # two options for test - test below would allow balance to be private if required
+   it 'returns increased balance when topped up' do
+     expect(card.top_up(8)).to eq 8
+   end
  end
 end
